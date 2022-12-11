5eeeb244d26e971bceada1fdaed69cdc83441c12
Feboy Kim <feboy-kim@outlook.com>
2022-12-12 00:51:48 +0800

    lease view to print

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -1,16 +1,21 @@
 import Head from "next/head"
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import Layout from "../../parts/layout"
 import { get } from "idb-keyval";
 import { InformationCircleIcon, PencilSquareIcon, PrinterIcon } from '@heroicons/react/24/outline'
 import LeaseView from "../../parts/lease-view"
+import { useReactToPrint } from "react-to-print";
 
 function View() {
     const router = useRouter()
     const { id } = router.query
     const [warning, setWarning] = useState("")
     const [lease, setLease] = useState()
+    const viewRef = useRef(null)
+    const onPrint = useReactToPrint({
+        content: () => viewRef.current
+    })
 
     useEffect(() => {
         const nId = Number(id)
@@ -39,7 +44,10 @@ function View() {
                 <InformationCircleIcon className="w-5 h-5 inline" />
                 <span className="inline align-middle pl-1">以下为合同正文：</span>
             </p>
-            {lease && <LeaseView lease={lease} />}
+            {lease && <div>
+                <LeaseView lease={lease} ref={viewRef} />
+                <button onClick={onPrint}>Print PDF</button>
+            </div>}
             <p className="p-2 m-2 border-t text-sm print:hidden opacity-75 max-w-4xl md:mx-auto">
                 <PrinterIcon className="w-5 h-5 inline" />
                 <span className="inline align-middle pl-1">使用浏览器中的打印功能或其他工具可以导出合同的PDF文件。</span>
